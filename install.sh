c4c3839363d6e1c95a63fd90f5cba975b3f151d2
Alucard <fire0lord0zuko@proton.me>
2026-01-10 18:39:24 +0200

    Update

--- install.sh
+++ install.sh
@@ -5,24 +5,33 @@ clangd_path="$PWD/clangd"
 kanata_path="$PWD/kanata/kanata.kbd"
 kitty_path="$PWD/kitty"
 nvim_path="$PWD/nvim"
+wezterm_path="$PWD/wezterm/.wezterm.lua"
+vscodium_path="$PWD/vscodium"
 
 bash_target="$HOME/.bashrc"
 clangd_target="$HOME/.config/clangd"
 kanata_target="$HOME/.config/kanata.kbd"
 kitty_target="$HOME/.config/kitty"
 nvim_target="$HOME/.config/nvim"
+wezterm_target="$HOME/.wezterm.lua"
+vscodium_target="$HOME/.config/VSCodium/User"
 
-echo "linking '$bash_path' to '$bash_target'"
-ln -snf $bash_path $bash_target
+function symlink {
+    echo "linking '$1' to '$2'"
+    ln -snf $1 $2
+}
 
-echo "linking '$clangd_path' to '$clangd_target'"
-ln -snf $clangd_path $clangd_target
+symlink $bash_path $bash_target
 
-echo "linking '$kanata_path' to '$kanata_target'"
-ln -snf $kanata_path $kanata_target
+symlink $clangd_path $clangd_target
 
-echo "linking '$kitty_path' to '$kitty_target'"
-ln -snf $kitty_path $kitty_target
+symlink $kanata_path $kanata_target
 
-echo "linking '$nvim_path' to '$nvim_target'"
-ln -snf $nvim_path $nvim_target
+symlink $kitty_path $kitty_target
+
+symlink $nvim_path $nvim_target
+
+symlink $wezterm_path $wezterm_target
+
+symlink $vscodium_path"/settings.json" $vscodium_target"/settings.json"
+symlink $vscodium_path"/keybindings.json" $vscodium_target"/keybindings.json"
