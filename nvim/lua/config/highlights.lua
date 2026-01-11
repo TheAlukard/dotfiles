7c1e4d5ff85a14117770cf232835dbea3354379c
Alucard <fire0lord0zuko@proton.me>
2026-01-12 01:31:35 +0200

    Update

--- nvim/lua/config/highlights.lua
+++ nvim/lua/config/highlights.lua
@@ -16,19 +16,24 @@ return {
         hl(0, "@keyword.repeat", {link = "@keyword"})
         hl(0, "@keyword.conditional", {link = "@keyword"})
         hl(0, "@keyword.directive.define", {link = "@keyword"})
-        hl(0, "@keyword", {fg = colors.keywords, force = true})
+
         hl(0, "@type.builtin", {link = "@type"})
         hl(0, "@lsp.type.macro", {link = "@type"})
         hl(0, "@type.definition", {link = "@type"})
-        hl(0, "@type", {fg = colors.types, force = true})
+
         hl(0, "@lsp.typemod.macro.globalScope.c", {link = "@constant"})
         hl(0, "@number", {link = "@constant"})
         hl(0, "@constant.builtin", {link = "@constant"})
-        hl(0, "@constant", {fg = colors.constants, force = true})
+
         hl(0, "@property", {link = "@variable"})
         hl(0, "@variable.parameter", {link = "@variable"})
-        hl(0, "@variable", {fg = colors.variables, force = true})
+
         hl(0, "@function.macro", {link = "@constant.macro"})
+
+        hl(0, "@keyword", {fg = colors.keywords, force = true})
+        hl(0, "@type", {fg = colors.types, force = true})
+        hl(0, "@constant", {fg = colors.constants, force = true})
+        hl(0, "@variable", {fg = colors.variables, force = true})
         hl(0, "@constant.macro", {fg = colors.macros, force = true})
         hl(0, "@function", {fg = colors.functions, force = true})
         hl(0, "@string", {fg = colors.strings, force = true})
