daff211cbd05ad0a399f71bc2d9e0ee32876d313
Alucard <fire0lord0zuko@proton.me>
2026-01-14 16:51:14 +0200

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,8 @@ if vim.g.neovide then
     vim.g.neovide_cursor_trail_size = 0.0
     vim.g.neovide_cursor_animate_command_line = false
     vim.g.neovide_cursor_animation_length = 0.0
+    vim.g.neovide_position_animation_length = 0.0
+    vim.g.neovide_scroll_animation_length = 0.1
     vim.opt.linespace = 1
     vim.g.neovide_scale_factor = 0.9
 end
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,10 @@
 return {
     "nvim-telescope/telescope.nvim",
     lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     opts = {
         defaults = {
             mappings = {
@@ -13,6 +17,9 @@ return {
             find_files = {
                 hidden = true,
                 no_ignore = true,
+                file_ignore_patterns = {
+                    ".git/",
+                },
             },
         },
     },
