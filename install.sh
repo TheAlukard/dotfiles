060f761945c777ec63075e4e8c78093844cbcfbf
Alucard <fire0lord0zuko@proton.me>
2026-01-23 01:33:31 +0200

    Add tmux

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ nvim_path="$DIR/nvim"
 wezterm_path="$DIR/wezterm/.wezterm.lua"
 vscodium_path="$DIR/vscodium"
 fastfetch_path="$DIR/fastfetch"
+tmux_path="$DIR/tmux"
 
 bash_target="$HOME/.bashrc"
 clangd_target="$HOME/.config/clangd"
@@ -25,6 +26,7 @@ nvim_target="$HOME/.config/nvim"
 wezterm_target="$HOME/.wezterm.lua"
 vscodium_target="$HOME/.config/VSCodium/User"
 fastfetch_target="$HOME/.config/fastfetch"
+tmux_target="$HOME/.config/tmux"
 
 slink $bash_path $bash_target
 
@@ -42,3 +44,5 @@ slink $vscodium_path"/settings.json" $vscodium_target"/settings.json"
 slink $vscodium_path"/keybindings.json" $vscodium_target"/keybindings.json"
 
 slink $fastfetch_path $fastfetch_target
+
+slink $tmux_path $tmux_target
