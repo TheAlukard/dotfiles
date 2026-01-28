75915a8edd66b2672f57fad880ab9d8b52b45b70
Alucard <fire0lord0zuko@proton.me>
2026-01-28 13:07:21 +0200

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -9,45 +9,25 @@ SCRIPT_PATH=$(realpath $0)
 DIR=$(dirname $SCRIPT_PATH)
 CONFIG="$HOME/.config"
 
-bash_path="$DIR/bash/.bashrc"
-clangd_path="$DIR/clangd"
-kanata_path="$DIR/kanata"
-kitty_path="$DIR/kitty"
-nvim_path="$DIR/nvim"
-wezterm_path="$DIR/wezterm/.wezterm.lua"
-vscodium_path="$DIR/VSCodium"
-fastfetch_path="$DIR/fastfetch"
-tmux_path="$DIR/tmux"
-sway_path="$DIR/sway"
+declare -a home=(
+    [0]='bash/.bashrc'
+    [1]='wezterm/.wezterm.lua'
+)
+declare -a config=(
+    [0]=clangd
+    [1]=kanata
+    [2]=kitty
+    [3]=nvim
+    [4]=VSCodium
+    [5]=fastfetch
+    [6]=tmux
+    [7]=sway
+)
 
-bash_target="$HOME/.bashrc"
-clangd_target="$HOME/.config/clangd"
-kanata_target="$HOME/.config/kanata"
-kitty_target="$HOME/.config/kitty"
-nvim_target="$HOME/.config/nvim"
-wezterm_target="$HOME/.wezterm.lua"
-vscodium_target="$HOME/.config/VSCodium"
-fastfetch_target="$HOME/.config/fastfetch"
-tmux_target="$HOME/.config/tmux"
-sway_target="$HOME/.config/sway"
+for i in "${home[@]}"; do
+    slink $(realpath $i) $HOME
+done
 
-slink $bash_path $bash_target
-
-slink $clangd_path $clangd_target
-
-slink $kanata_path $kanata_target
-
-slink $kitty_path $kitty_target
-
-slink $nvim_path $nvim_target
-
-slink $wezterm_path $wezterm_target
-
-slink $vscodium_path"/settings.json" $vscodium_target"/settings.json"
-slink $vscodium_path"/keybindings.json" $vscodium_target"/keybindings.json"
-
-slink $fastfetch_path $fastfetch_target
-
-slink $tmux_path $tmux_target
-
-slink $sway_path $sway_target
+for i in "${config[@]}"; do
+    slink $(realpath $i) $CONFIG
+done
