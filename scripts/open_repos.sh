bb18d28d0b5edbc1193190032602d826b7dcd0cf
Alucard <fire0lord0zuko@proton.me>
2026-02-06 20:13:57 +0200

    Move type argument after min and max depth

--- scripts/open_repos.sh
+++ scripts/open_repos.sh
@@ -1,3 +1,3 @@
-repo_path=$(find ~/programming/repos -type d -mindepth 1 -maxdepth 1 | fzf)
+repo_path=$(find ~/programming/repos -mindepth 1 -maxdepth 1 -type d | fzf)
 repo_name=$(basename "$repo_path")
 tmux new -AD -s "$repo_name" -c "$repo_path"
