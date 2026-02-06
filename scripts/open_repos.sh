3ea3c56becfa631a0918d9e35f0c575bf1f385a1
Alucard <fire0lord0zuko@proton.me>
2026-02-06 16:08:23 +0200

    Use '-A' flag instead of attaching separately

--- scripts/open_repos.sh
+++ scripts/open_repos.sh
@@ -1,4 +1,3 @@
 repo_path=$(find ~/programming/repos -mindepth 1 -maxdepth 1 | fzf)
 repo_name=$(basename "$repo_path")
-tmux new -s "$repo_name" -c "$repo_path"
-tmux attach -d -t "$repo_name"
+tmux new -A -s "$repo_name" -c "$repo_path"
