473005156052ee4187256302bf6993fb7927c24a
Alucard <fire0lord0zuko@proton.me>
2026-02-06 16:13:48 +0200

    Add 'D' flag

--- scripts/open_repos.sh
+++ scripts/open_repos.sh
@@ -1,3 +1,3 @@
 repo_path=$(find ~/programming/repos -mindepth 1 -maxdepth 1 | fzf)
 repo_name=$(basename "$repo_path")
-tmux new -A -s "$repo_name" -c "$repo_path"
+tmux new -AD -s "$repo_name" -c "$repo_path"
