095b8828c84b5ac67a47775d28cebdf1ea0cb0be
Alucard <fire0lord0zuko@proton.me>
2026-02-08 17:53:54 +0200

    Check if repo exists before continuing

--- scripts/open_repos.sh
+++ scripts/open_repos.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
 repo_path=$(find ~/programming/repos -mindepth 1 -maxdepth 1 -type d | fzf)
-repo_name=$(basename "$repo_path")
-tmux new -AD -s "$repo_name" -c "$repo_path"
+
+if [[ $repo_path ]]; then
+    repo_name=$(basename "$repo_path")
+    tmux new -AD -s "$repo_name" -c "$repo_path"
+fi
