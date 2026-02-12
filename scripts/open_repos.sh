355d10266ac559b094465645a6a02ae4b30111c3
Alucard <fire0lord0zuko@proton.me>
2026-02-12 13:44:16 +0200

    Update

--- scripts/open_repos.sh
+++ scripts/open_repos.sh
@@ -4,5 +4,5 @@ repo_path=$(find ~/programming/repos -mindepth 1 -maxdepth 1 -type d | fzf)
 
 if [[ $repo_path ]]; then
     repo_name=$(basename "$repo_path")
-    tmux new -AD -s "$repo_name" -c "$repo_path"
+    tmux new -ADs "$repo_name" -c "$repo_path"
 fi
